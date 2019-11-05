6066175fa460dc1f551efe3b8cedd31704ac1870
davidagitlen <davidagitlen@gmail.com>
2019-11-05 13:05:40 -0700

    Include check for repsonse.ok and length of returned data for throwing Error objects

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -7,8 +7,14 @@ const apiKey = process.env.VUE_APP_API_KEY;
 export const getSynonyms = async (word) => {
   const url = urlStart + `/${word}?key=${apiKey}`;
   const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Sorry, we couldn\'t find the word you were looking for! Please enter a new word.');
+  }
   console.log(response)
   const data = await response.json();
+  if (!data.length) {
+    throw new Error('Sorry, we couldn\'t find the word you were looking for! Please enter a new word.')
+  }
   console.log('data', data)
   return data[0];
 }
